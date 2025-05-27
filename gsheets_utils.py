007854b6f1b2b1e01bb928c0dd507f475c273e8e
Moedjaheed <muhwmmadridho@gmail.com>
2025-05-27 15:47:36 +0700

    rev : gsheet_utils

--- gsheets_utils.py
+++ gsheets_utils.py
@@ -1,17 +1,19 @@
-
 import gspread
+import json
+import pandas as pd
+import streamlit as st
 from gspread_dataframe import get_as_dataframe, set_with_dataframe
 from oauth2client.service_account import ServiceAccountCredentials
-import pandas as pd
 
 # Fungsi autentikasi dan pembacaan dari Google Sheets
-def load_gsheet(sheet_id, worksheet_name, cred_path="aldzama-dashboard-hidrolis-415739989ae4.json"):
+def load_gsheet(sheet_id, worksheet_name):
     scope = [
         "https://spreadsheets.google.com/feeds",
         "https://www.googleapis.com/auth/spreadsheets",
         "https://www.googleapis.com/auth/drive"
     ]
-    creds = ServiceAccountCredentials.from_json_keyfile_name(cred_path, scope)
+    creds_dict = json.loads(st.secrets["gcp_service_account"])
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
     client = gspread.authorize(creds)
     worksheet = client.open_by_key(sheet_id).worksheet(worksheet_name)
     df = get_as_dataframe(worksheet).dropna(how='all')
